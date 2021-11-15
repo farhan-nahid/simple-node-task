687a037d50823edf1e2a8ded2f873d66b4827a58
gesora96 <nahidahmed9933@gmail.com>
2021-11-15 17:27:24 +0600

    add product done

--- src/components/AddProduct/AddProduct.js
+++ src/components/AddProduct/AddProduct.js
@@ -1,29 +1,65 @@
+import axios from 'axios';
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
 
 const AddProduct = () => {
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+
+  const onSubmit = (data) => {
+    axios.post('http://localhost:5000/add-products', data).then((res) => {
+      if (res.status === 200) {
+        reset();
+        alert('Product Added In Our Database');
+      } else {
+        alert('Something Went wrong!!. Please try again later');
+      }
+    });
+  };
 
   return (
-    <div>
+    <Container className='mt-5 pt-5 text-center'>
       <h1>This is Add Product</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        {/* register your input into the hook by invoking the "register" function */}
-        <input defaultValue='test' {...register('example')} />
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className='p-5 border rounded shadow mt-5 w-50 mx-auto'
+      >
+        <input
+          placeholder='Product Name'
+          {...register('name', { required: true })}
+          className='form-control'
+          type='text'
+        />
+        {errors.name && (
+          <span className='text-danger'>This field is required</span>
+        )}
+        <input
+          placeholder='Product Price'
+          {...register('price', { required: true })}
+          className='form-control my-3'
+          type='number'
+        />
+        {errors.price && (
+          <span className='text-danger'>This field is required</span>
+        )}
+        <input
+          placeholder='Product Quantity'
+          {...register('quantity', { required: true })}
+          className='form-control my-3'
+          type='number'
+        />
+        {errors.quantity && (
+          <span className='text-danger'>This field is required</span>
+        )}
 
-        {/* include validation with required or other standard HTML validation rules */}
-        <input {...register('exampleRequired', { required: true })} />
-        {/* errors will return when field validation fails  */}
-        {errors.exampleRequired && <span>This field is required</span>}
-
-        <input type='submit' />
+        <input type='submit' className='btn btn-primary d-block' />
       </form>
-    </div>
+    </Container>
   );
 };
 
